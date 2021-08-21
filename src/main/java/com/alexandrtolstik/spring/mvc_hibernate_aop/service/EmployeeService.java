6d3134f719f6f41a1846a8925c29b99316703ddc
Alexandr <francuzby@gmail.com>
2021-08-22 00:52:18 +0300

    @Service

---
+++ src/main/java/com/alexandrtolstik/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.alexandrtolstik.spring.mvc_hibernate_aop.service;
+
+import com.alexandrtolstik.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+}
