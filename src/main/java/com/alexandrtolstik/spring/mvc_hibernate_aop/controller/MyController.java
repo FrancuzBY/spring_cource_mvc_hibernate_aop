73de9d1f82ec9a21676a9788dbbbe01451dc6c5d
Alexandr <francuzby@gmail.com>
2021-08-23 14:51:01 +0300

    Spring MVC + Hibernate + AOP

--- src/main/java/com/alexandrtolstik/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/alexandrtolstik/spring/mvc_hibernate_aop/controller/MyController.java
@@ -4,6 +4,7 @@ import com.alexandrtolstik.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import com.alexandrtolstik.spring.mvc_hibernate_aop.entity.Employee;
 import com.alexandrtolstik.spring.mvc_hibernate_aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class MyController {
 
     @Autowired
